c84cfc8f9486e7a1f077f9d77608f73ab8e343af
OwenKane <owen.a.kane@gmail.com>
2017-01-30 20:29:11 +0000

    Continued work on registration

--- SecureDoc/accounts/views.py
+++ SecureDoc/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -10,10 +10,12 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User name has been taken'})
             except User.DoesNotExist:
-                User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                login(request, user)
                 return render(request, 'accounts/signup.html')
         else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords didn\'t match '})
+            return render(request, 'accounts/signup.html', {'error': 'Passwords didn\'t match'})
     else:
         return render(request, 'accounts/signup.html')
