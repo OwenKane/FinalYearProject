a9d6ce6041575154b4bf37744b2c5ff8f25ec3c7
OwenKane <owen.a.kane@gmail.com>
2017-01-31 17:30:55 +0000

    Added ability to add 'posts'

--- SecureDoc/posts/models.py
+++ SecureDoc/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    document = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(User)
+    nominated = models.TextField()
--- SecureDoc/posts/views.py
+++ SecureDoc/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from . import models
 
 
 # Create your views here.
@@ -7,4 +9,15 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def create(request):
-    return render(request, 'posts/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['document']:
+            post = models.Post()
+            post.title = request.POST['title']
+            post.document = request.POST['document']
+            post.pub_date = timezone.datetime.now()
+            post.author = request.user
+            post.save()
+        else:
+            return render(request, 'posts/create.html', {'error': 'Error: Need to fill in all fields'})
+    else:
+        return render(request, 'posts/create.html')
