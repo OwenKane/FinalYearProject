d7ef9a4acb9fba63666038dbdb6a4fd5653f914b
OwenKane <owen.a.kane@gmail.com>
2017-02-01 17:13:09 +0000

    Added homepage

--- SecureDoc/posts/views.py
+++ SecureDoc/posts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 
 
 # Create your views here.
@@ -11,13 +11,19 @@ from . import models
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['document']:
-            post = models.Post()
+            post = Post()
             post.title = request.POST['title']
             post.document = request.POST['document']
             post.pub_date = timezone.datetime.now()
             post.author = request.user
             post.save()
+            return redirect('home')
         else:
             return render(request, 'posts/create.html', {'error': 'Error: Need to fill in all fields'})
     else:
         return render(request, 'posts/create.html')
+
+
+def home(request):
+    posts = Post.objects.order_by('pub_date')
+    return render(request, 'posts/home.html', {'posts':posts})
