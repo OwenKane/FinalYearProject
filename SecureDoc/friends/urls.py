767127ca58e8d4b98db489efee90505e6310b165
OwenKane <owen.a.kane@gmail.com>
2017-02-11 21:50:37 +0000

    Users can not delete friends and reply to friend requests

--- SecureDoc/friends/urls.py
+++ SecureDoc/friends/urls.py
@@ -7,4 +7,7 @@ app_name = 'friends'
 
 urlpatterns = [
     url(r'^view_friends/', views.view_friends, name="view_friends"),
+    url(r'^delete_friend/', views.delete_friend, name="delete_friend"),
+    url(r'^confirm_friend/', views.confirm_friend, name="confirm_friend"),
+    url(r'^deny_friend/', views.deny_friend, name="deny_friend"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SecureDoc/friends/views.py
+++ SecureDoc/friends/views.py
@@ -7,13 +7,42 @@ from .models import User
 # Create your views here.
 @login_required
 def view_friends(request):
+    users, friend_req = get_friend_info(request)
+    return render(request, 'friends/view_friends.html', {'users': users, 'friend_req': friend_req})
+
+
+def delete_friend(request):
+    current_user = request.user
+    del_user = request.POST['rm_friend_id']
+    Friend.objects.filter(friend_id=del_user).filter(user_id=current_user.id).delete()
+    users, friend_req = get_friend_info(request)
+    return render(request, 'friends/view_friends.html', {'users': users, 'friend_req': friend_req})
+
+
+def get_friend_info(request):
     current_user = request.user
     friend_id = Friend.objects.filter(user_id=current_user.id, pending=False)
     friend_req_id = Friend.objects.filter(user_id=current_user.id, pending=True)
     users = []
     friend_req = []
     for f in friend_id:
-        users.append(User.objects.filter(id=f.friend_id).values('username'))
+        users.append(User.objects.filter(id=f.friend_id).values('username', 'id'))
     for r in friend_req_id:
-        friend_req.append(User.objects.filter(id=r.friend_id).values('username'))
+        friend_req.append(User.objects.filter(id=r.friend_id).values('username', 'id'))
+    return users, friend_req
+
+
+def confirm_friend(request):
+    current_user = request.user
+    req_friend = request.POST['req_friend_id']
+    Friend.objects.filter(friend_id=req_friend).filter(user_id=current_user.id).update(pending=False)
+    users, friend_req = get_friend_info(request)
+    return render(request, 'friends/view_friends.html', {'users': users, 'friend_req': friend_req})
+
+
+def deny_friend(request):
+    current_user = request.user
+    del_user = request.POST['req_friend_id']
+    Friend.objects.filter(friend_id=del_user).filter(user_id=current_user.id).delete()
+    users, friend_req = get_friend_info(request)
     return render(request, 'friends/view_friends.html', {'users': users, 'friend_req': friend_req})
